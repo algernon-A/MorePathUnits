2a5dcf2a0b183c7e13062b9153d0d49e9fc4791d
algernon-A <aeneas@aloku.net>
2022-01-04 01:41:49 +1100

    Update translation framwork.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -10,7 +10,7 @@ namespace MorePathUnits
     public class MPUMod : IUserMod
     {
         public static string ModName => "More PathUnits";
-        public static string Version => "0.0.4";
+        public static string Version => "0.0.5";
 
         public string Name => ModName + " " + Version;
         public string Description => Translations.Translate("MPU_DESC");
